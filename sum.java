d93f4fd489d12276f0d023fdc1a99fab4b96f4f7
Ayesha Arshad <fsports417@gmail.com>
2026-05-22 15:33:01 +0500

    add mul.java

--- sum.java
+++ sum.java
@@ -1,14 +1,10 @@
-import java.util.Scanner;
+//import java.util.Scanner;
 
 public class sum {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter first number: ");
-        double a = scanner.nextDouble();
-        System.out.print("Enter second number: ");
-        double b = scanner.nextDouble();
-        double sum = a + b;
-        System.out.println("Sum = " + sum);
-        scanner.close();
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("the sum is: " + sum );
     }
 }
